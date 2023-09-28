f93d13fa28c13f6e80b0ee121ff648240b37a684
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-09-28 11:26:34 +0700

    Context & loading

--- src/App.js
+++ src/App.js
@@ -10,27 +10,37 @@ import Blog from "./components/pages/blog";
 import Contact from "./components/pages/contact";
 import Category from "./components/pages/category";
 import Product from "./components/pages/products/product";
+import { useState } from "react";
+import STATE from "./context/initState";
+import { UserProvider } from "./context/context";
 
 function App() {
+    const [state, setState] = useState(STATE);
     return (
-        <div className="App">
-            <Header />
-            <Hero />
-            <Breadcrumbs />
-            <section className="product spad">
-                <div className="container">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/shop" element={<Shop />} />
-                        <Route path="/category/:id" element={<Category />} />
-                        <Route path="/product/:id" element={<Product />} />
-                        <Route path="/blog" element={<Blog />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
-                </div>
-            </section>
-            <Footer />
-        </div>
+        <UserProvider value={{ state, setState }}>
+            <div id="preloder" style={{ opacity: 0.8, display: state.loading ? "block" : "none" }}>
+                <div class="loader"></div>
+            </div>
+
+            <div className="App">
+                <Header />
+                <Hero />
+                <Breadcrumbs />
+                <section className="product spad">
+                    <div className="container">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/shop" element={<Shop />} />
+                            <Route path="/category/:id" element={<Category />} />
+                            <Route path="/product/:id" element={<Product />} />
+                            <Route path="/blog" element={<Blog />} />
+                            <Route path="/contact" element={<Contact />} />
+                        </Routes>
+                    </div>
+                </section>
+                <Footer />
+            </div>
+        </UserProvider>
     );
 }
 
